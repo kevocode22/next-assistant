f4febf3131d1a20819e38e1181339bdd17a0810d
kevocode22 <ocampokevin14@gmail.com>
2023-07-10 09:06:20 -0300

    build: :sparkles: react-scroll
    npm i react-scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import '@styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/logo.png" type="image/x-icon" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 import useLLM from "usellm";
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
   const llm = useLLM({ serviceUrl: "https://usellm.org/api/llm" });
   const [prompt, setPrompt] = useState("");
   const [result, setResult] = useState("");
-  async function handleChange(e:any) {
+  const [copied, setCopied] = useState("");
+
+  async function handleChange(e: any) {
     e.preventDefault();
     try {
       await llm.chat({
@@ -25,27 +28,60 @@ export default function Home() {
     }
     setPrompt("");
   }
+
+  const handleCopy = () => {
+    setCopied(result);
+    navigator.clipboard.writeText(result);
+    setTimeout(() => setCopied("false"), 3000);
+  };
+
   return (
-    <div className="min-h-screen mx-auto my-8 max-w-4xl">
-      <h1 className="font_title text-center mb-4 text-2xl">Next Assistant AI</h1>
-      <div className="flex justify-center items-center">
-        <form method="post" onSubmit={handleChange}>
-          <input
-            id="input-next"
-            value={prompt}
-            onChange={(e) => setPrompt(e.target.value)}
-            placeholder="Enter your prompt..."
-            className="rounded border p-2 mr-2 text-black w-full"
+    <div className="my-8 max-w-screen flex justify-center items-center flex-col">
+      <div className="space-y-28 py-30">
+        <div className="flex justify-center items-center flex-col w-full">
+          <Image
+            className="mr-2"
+            src="/logo.png"
+            width={30}
+            height={30}
+            alt="Next Assistant Logo"
           />
-          <button
-            className="rounded border border-black dark:border-white p-2"
-            type="submit"
-          >
-            Submit
-          </button>
-        </form>
+          <h1 className="font-logo">Next Assistant AI</h1>
+
+          <p className="font_title">Discover the power of advanced AI</p>
+          <p className="desc">
+            Obtain precise and relevant answers to your queries within seconds
+          </p>
+
+          <p className="desc"></p>
+        </div>
+        <div className="flex justify-center items-center">
+          <form method="post" onSubmit={handleChange} className="flex w-full">
+            <input
+              id="input-next"
+              value={prompt}
+              onChange={(e) => setPrompt(e.target.value)}
+              placeholder="Write your querie..."
+              className="rounded border-black p-2 mr-2 text-black w-full shadow-2xl "
+            />
+            <button
+              className="rounded w-24 text-white font-medium bg-black"
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
-      <div className="mt-4 whitespace-pre-wrap">{result}</div>
+      <div className="mt-8 whitespace-pre-wrap w-7/12">
+
+        <div className="cursor-pointer flex justify-between" onClick={handleCopy}>
+          <p className="font-semibold pl-30">Next Assistant:</p>
+          {copied == result ? "✅" : "📋" }
+        
+        </div>
+        <p className="text-center">{result}</p>
+      </div>
     </div>
   );
 }
